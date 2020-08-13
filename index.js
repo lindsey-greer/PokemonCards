a26a31f64e2e70d8f74aedb7adecd5462973603c
Lindsey Greer <13lgreer#gmail.com>
2020-08-12 22:06:37 -0600

    created loadpokemon function

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ addButton.addEventListener('click', ()=>{
     addPokemon()
 })
 
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = response.json()
+        return data
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
+function loadPokemon() {
+    getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
+        (data) => {
+            for (const pokemon of data.results) {
+                getAPIData(pokemon.url).then(
+                    (pokeData) => {
+                        populatePokeCard(pokeData)
+                    }
+                )
+            }
+        }
+    )
+}
