ef0956de42b141df9d946e230744f96c35637fcd
Lindsey Greer <13lgreer#gmail.com>
2020-08-12 18:24:17 -0600

    added card css

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 let pokeContainer = document.querySelector('.pokeContainer')
+let viewButton = document.querySelector('#viewButton')
+viewButton.addEventListener('click',() =>) {
+    loadPage()
+})
+
+let addButton = document.querySelector('#addButton')
+addButton.addEventListener('click', () => ) {
+    addPokemon ()
+})
+
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = response.json()
+        return data
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
